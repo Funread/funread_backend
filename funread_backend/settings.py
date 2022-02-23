753b6929b3f7fe66caf5422f0a7c95d10b91e85f
914-Chu <jujoyce12913804@gmail.com>
2022-02-23 14:03:21 -0600

    update: tested teacher crud with new db

--- funread_backend/settings.py
+++ funread_backend/settings.py
@@ -11,27 +11,31 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from smtpd import DebuggingServer
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import environ
+
 # Initialise environment variables
 env = environ.Env()
+
+# Take environment variables from .env file
 environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z*q#84&=4o14&)-z@uy-*dox9%_o7@%e*ls3obq8@8$u^m+5x9'
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 #TODO: unsecure
 ALLOWED_HOSTS = ['*']
-
-
 
 # Application definition
 
@@ -102,7 +106,7 @@ DATABASES = {
         'ENGINE': 'djongo', #pymongo == 3.12.1 otherwise raise error
         'CLIENT': {
             'host': env('DATABASE_HOST'),
-            'name': '',
+            'name': env('DATABASE_NAME'),
             'authMechanism': 'SCRAM-SHA-1' # for cloud db
         }
     }
