81e68c7f889c651abd90d9d86c412d13d40cb419
jinhyuk4 <jinhyuk4>
2022-03-02 01:55:59 -0600

    update: add basic login/logout

--- AuthApp/views.py
+++ AuthApp/views.py
@@ -2,5 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def login_view(request):
+
+  username = request.POST['username']
+  password = request.POST['password']
+  user = authenticate(request, username=username, password=password)
+
+  if user is not None:
+    login(request, user)
+    # just temporary; use rest framework to get correct status code later
+    return HttpResponse('<h1>Login Success</h1>')
+  else:
+    # just temporary; change this to 401 error later (using rest framework)
+    return HttpResponseNotFound('<h1>Login failed</h1>')
+
+@csrf_exempt
+def logout_view(request):
+  logout(request)
+  
+ 
