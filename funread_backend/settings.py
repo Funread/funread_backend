fc4519715ec1ffa9d9d6726d37247206dfe927c9
Bingji Guo <guobingji1998@icloud.com>
2022-03-03 01:38:33 -0600

    update: add whitenoise to serve static files

--- funread_backend/settings.py
+++ funread_backend/settings.py
@@ -65,11 +65,13 @@ INSTALLED_APPS = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = [
+    "localhost",
     "funread-backend.herokuapp.com",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,8 +154,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static") 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
