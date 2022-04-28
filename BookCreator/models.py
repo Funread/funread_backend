5c5e2a0f6835fcbd4b0856e0844d47569837f410
jinhyuk4 <89655693+jinhyuk4@users.noreply.github.com>
2022-04-28 18:24:30 -0500

    book api

--- BookCreator/models.py
+++ BookCreator/models.py
@@ -6,11 +6,15 @@ class Page(models.Model):
   subtitle = models.CharField(max_length=100)
   content = models.CharField(max_length=100)
 class BookCreator(models.Model):
+   #TODO from oscar: add UnitName field
   id = models.CharField(max_length=100, primary_key=True)
   name = models.CharField(max_length=100)
   type = models.CharField(max_length=100)
   teacherId = models.CharField(max_length=100)
   grade = models.IntegerField()
   contents = models.ArrayField(model_container=Page, )
+
+
+ 
 
 
